fbbcaca2cd99850630c3d7110e7812492bd94b5a
Nick <nick@smplfi.com>
2025-04-05 11:26:18 -0400

    shortened default command to DBA, added configuration for optional port, streamlined op selection logic for future development and better error handling, changed default database field to database instead of dbname

--- lua/dadbod-auth/config.lua
+++ lua/dadbod-auth/config.lua
@@ -8,7 +8,7 @@ config.options = {
 function config.setup(user_opts)
 	config.options = vim.tbl_deep_extend("force", config.options, user_opts or {})
 
-	vim.api.nvim_create_user_command("DBConnect", function(opts)
+	vim.api.nvim_create_user_command("DBA", function(opts)
 		require("dadbod-auth").setup_db_connection(opts.args)
 	end, {
 		nargs = 1, -- requires an argument (the name of the 1Password item)
--- lua/dadbod-auth/init.lua
+++ lua/dadbod-auth/init.lua
@@ -24,16 +24,16 @@ local function resolve_item_name(alias_or_item)
 end
 
 local function normalize_type(type)
+    if not type then return nil end
     local remove_spaces = string.gsub(type, "[^%w_]", "")
     return string.lower(remove_spaces)
 end
 
 local function fetch_db_credentials(item_name)
     local ext = config.options.force_exe and '.exe' or ''
-	local opcmd = string.format('op%s item get "', ext)
-		.. item_name
-		.. '" --fields label=type,label=username,label=password,label=dbname,label=server --format json'
+    local opcmd = string.format("op%s item get %q --format json", ext, item_name)
 	local handle = io.popen(opcmd)
+    if not handle then return nil end
 	local result = handle:read("*a")
 	handle:close()
 
@@ -49,46 +49,51 @@ local function fetch_db_credentials(item_name)
 		return nil
 	end
 
-	local type = credential_data[1].value
-	local username = credential_data[2].value
-	local password = credential_data[3].value
-	local dbname = credential_data[4].value
-	local host = credential_data[5].value
+    local credentials = {}
 
-    -- Check if database type is valid
-    local nt = normalize_type(type)
-    local type_data = config.options.custom_types[nt]
-    if type_data == nil then type_data = default_types[nt] end
-    if type_data == nil then
-        vim.notify("No adapter info found for database type:" .. type, vim.log.levels.ERROR)
-        return nil
+    if credential_data.fields then
+        for _, field in ipairs(credential_data.fields) do
+            if field.label and field.value then
+                credentials[field.label] = field.value
+            end
+        end
     end
 
-	return {
-		type = type_data,
-		username = username,
-		password = password,
-		dbname = dbname,
-		host = host,
-	}
+    return credentials
+end
+
+local function get_type_data(type_string)
+    local nt = normalize_type(type_string)
+    local data = config.options.custom_types[nt]
+    if data == nil then data = default_types[nt] end
+    return data
 end
 
 function M.setup_db_connection(item_name)
 	local resolved_item_name = resolve_item_name(item_name)
 	local creds = fetch_db_credentials(resolved_item_name)
-	if not creds then
-		return
-	end
-    local db_string = string.format("%s://", creds.type.header)
+	if not creds then return end
+    local type_data = get_type_data(creds.type)
+    if type_data == nil then
+        vim.notify("No adapter info found for database type: " .. type, vim.log.levels.ERROR)
+        return
+    end
+
+    local db_string = string.format("%s://", type_data.header)
     local server_prefix = ""
-    if not creds.type.suppress_user then
+    if not type_data.suppress_user and creds.username then
         db_string = db_string .. creds.username
         server_prefix = '@'
     end
-    if creds.type.pw_env_var then vim.env[creds.type.pw_env_var] = creds.password
-    elseif not creds.type.suppress_pw  then db_string = db_string .. ':' .. url_encode(creds.password) end
-    db_string = db_string .. string.format("%s%s/%s", server_prefix, creds.host, creds.dbname)
-    if creds.type.params then db_string = db_string .. '?' .. creds.type.params end
+    if creds.password then
+        if type_data.pw_env_var then vim.env[type_data.pw_env_var] = creds.password
+        elseif not type_data.suppress_pw  then db_string = db_string .. ':' .. url_encode(creds.password) end
+    end
+    db_string = db_string .. string.format("%s%s/%s", server_prefix, creds.host, creds.database)
+    if creds.port then
+        db_string = db_string .. string.format(":%s", creds.port)
+    end
+    if type_data.params then db_string = db_string .. '?' .. type_data.params end
 	-- Set the connection for vim-dadbod
 	vim.t.db = db_string
 	vim.notify("database credentials configured!", vim.log.levels.INFO)
